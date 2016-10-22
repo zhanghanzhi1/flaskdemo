05d064f7171c5c1cd2e94be5e2dd3b1c778cf7cc
Lindsay Ward <lindsay.ward@jcu.edu.au>
2016-10-22 14:47:51 +1000

    Add templates with inheritance for layout and navigation, basic home page, fix page title search

--- flaskdemo.py
+++ flaskdemo.py
@@ -3,16 +3,22 @@ import wikipedia
 
 app = Flask(__name__)
 # set the secret key.  keep this really secret:
-app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+app.secret_key = 'A0Zr98j/3yXa R~XHH!jmN]LWX/,?RT'
+
+
+@app.route('/test')
+def test():
+    return 'Hello World!'
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template("home.html")
 
 
 @app.route('/about')
 def about():
-    return "I am me"
+    return "I am still working on this"
 
 
 @app.route('/hello')
@@ -44,9 +50,15 @@ def get_page(search_term):
         page = wikipedia.page(wikipedia.random())
     except wikipedia.exceptions.DisambiguationError:
         # this is a disambiguation page, get the first real page (close enough)
-        pages = wikipedia.search(search_term)
-        page = wikipedia.page(pages[1])
+        page_titles = wikipedia.search(search_term)
+        # sometimes the next page has the same name (different caps), so don't try the same again
+        if page_titles[1].lower() == page_titles[0].lower():
+            title = page_titles[2]
+        else:
+            title = page_titles[1]
+        page = get_page(wikipedia.page(title))
     return page
+
 
 if __name__ == '__main__':
     app.run()
